5fb007b2d29740a79c3edf2e41b952ed0fd53921
asmmcyber <asmita735pal@gmail.com>
2024-08-27 17:35:27 +0530

    Alumni-profile

--- FrontEnd/job-portal.js
+++ FrontEnd/job-portal.js
@@ -1,12 +1,13 @@
-// Initialize the map
-var map = L.map('map').setView([51.505, -0.09], 13); // Default location set to London
+// Initialize the map centered on India
+var map = L.map('map').setView([20.5937, 78.9629], 5); // Coordinates for the center of India
 
 // Load the tile layer from OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Add a marker to the map
-L.marker([51.505, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+// Add a marker to the map (example location: New Delhi)
+L.marker([28.6139, 77.2090]).addTo(map) // Coordinates for New Delhi
+    .bindPopup('New Delhi, the capital of India.')
     .openPopup();
+
